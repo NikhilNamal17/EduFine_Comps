86035338c080a3d75bbe2a6c92a8f1fa4039919c
NikhilNamal17 <nikhilnamal16@gmail.com>
2020-01-12 18:38:08 +0530

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: `EduLearn`,
+    name: `Nikhil Namal`,
+    siteUrl: `https://gatsby-theme-document.netlify.com`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/NikhilNamal17`
+      },
+      {
+        name: `twitter`,
+        url: `https://twitter.com/Nikhil_namal17`
+      }
+    ],
+    sidebarConfig: {
+      forcedNavOrder: ["/Distributed Systems Contents", " / Computers ", " / EXTC"],
+      ignoreIndex: false
+    }
+  },
+  plugins: [{ resolve: `gatsby-theme-document` },
+  ]
+};
